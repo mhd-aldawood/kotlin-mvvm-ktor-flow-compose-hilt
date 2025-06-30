d5cfb283deb53d33058850cc617cc9e737a857ec
Mohamad-Mob-Dev <amohamad@darasa.io>
2025-06-30 20:24:36 +0400

    font,typograph

---
+++ app/src/main/java/com/example/kotlintest/ui/theme/Typograph.kt
@@ -0,0 +1,49 @@
+package com.example.kotlintest.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+//list of all available typegraph will be used in the project
+//val Typography.sfMonoItalic: TextStyle
+//    @Composable
+//    get() = TextStyle(
+//        fontWeight = FontWeight.Normal,
+//        fontSize = 12.sp,
+//        fontFamily = FontSFPro(),
+//        fontStyle = FontStyle.Italic,
+//        lineHeight = 14.sp
+//    )
+//
+//val Typography.sfMonoItalicLight: TextStyle
+//    @Composable
+//    get() = TextStyle(
+//        fontWeight = FontWeight.Light,
+//        fontSize = 12.sp,
+//        fontFamily = FontSFMonoLightItalic(),
+//        fontStyle = FontStyle.Italic,
+//        lineHeight = 12.sp
+//    )
+//
+//val Typography.sfMonoTF: TextStyle
+//    @Composable
+//    get() = TextStyle(
+//        color = MaterialTheme.colorScheme.graysGray,
+//        fontSize = 14.sp, // Adjust font size as needed
+//        fontWeight = FontWeight(300),
+//        lineHeight = 24.sp,
+//        fontFamily = FontSFMono()
+//    )
+//
+//val Typography.sfMonoTFWarning: TextStyle
+//    @Composable
+//    get() = TextStyle(
+//        color = MaterialTheme.colorScheme.graysGray,
+//        fontSize = 12.sp, // Adjust font size as needed
+//        fontWeight = FontWeight(400),
+//        lineHeight = 14.32.sp,
+//        fontFamily = FontSFMono()
+//    )
